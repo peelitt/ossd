bd0d510f5080c31ece6114f08975c341aaab9fbe
say <ayoung07275@gmail.com>
2023-11-24 02:43:37 +0900

    ok

--- Mobius-master/mobiusmqtt.js
+++ Mobius-master/mobiusmqtt.js
@@ -9,24 +9,23 @@ const topic = 'data';
 
 // MQTT 클라이언트 생성
 //const client = mqtt.connect(`${brokerUrl}:${brokerPort}`);
-
 // MQTT 브로커에 연결
 const client = mqtt.connect('ws://localhost:9001'); // 웹소켓 주소에 맞게 수정
 
 // 연결이 성공하면 데이터를 주기적으로 전송
 client.on('connect', () => {
   setInterval(() => {
-    /*const data = {
+    /*
+    const data = {
       temperature: Math.random() * 30, // 예시 데이터 (온도)
       humidity: Math.random() * 80,    // 예시 데이터 (습도)
     };
     */
-
-    const temperature= Math.random() * 30
-    //소수점까지 나오니 정수로 하려면 약간의 수정 필요~
+   const temperature= Math.random() * 30
 
     // 데이터를 JSON 문자열로 변환하여 MQTT 토픽에 전송
     const message = Buffer.from(String(temperature));
+
     client.publish(topic, message);
 
     console.log('Data published:', temperature);
