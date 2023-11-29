0d7366007f47375c2c41bda5bd2539e8c4ef1e66
HalinLee <sejonglee@sju.co.kr>
2023-11-29 17:14:09 +0900

    trivial change in getVisibility.py

--- oneM2M-Device_Simulator/getVisibility.py
+++ oneM2M-Device_Simulator/getVisibility.py
@@ -1,55 +1,48 @@
 import requests
-from ..  import app
 from bs4 import BeautifulSoup
 
-# oneM2M 서버 설정
-cseURL = "http://127.21.48.1:7579/Mobius2"
-cseRelease = "1"  # release 값이 필요하면 설정, 필요 없으면 빈 문자열로 남겨둘 수 있습니다.
+# Constant for the desired city
+DESIRED_CITY = "서울"
 
-# 크롤링할 사이트의 URL
-url = "https://www.weather.go.kr/w/obs-climate/land/city-obs.do"
+def get_visibility():
+    # 크롤링할 사이트의 URL
+    url = "https://www.weather.go.kr/w/obs-climate/land/city-obs.do"
 
-# requests를 사용하여 페이지 내용 가져오기
-response = requests.get(url)
-html = response.text
+    # requests를 사용하여 페이지 내용 가져오기
+    response = requests.get(url)
+    html = response.text
 
-# BeautifulSoup을 사용하여 HTML 파싱
-soup = BeautifulSoup(html, "html.parser")
+    # BeautifulSoup을 사용하여 HTML 파싱
+    soup = BeautifulSoup(html, "html.parser")
 
-# 원하는 테이블 선택
-target_table = soup.find("table", class_="table-col")
+    # 원하는 테이블 선택
+    target_table = soup.find("table", class_="table-col")
 
-# 테이블의 모든 행(rows)을 가져오기
-rows = target_table.find_all("tr")
+    # 테이블의 모든 행(rows)을 가져오기
+    rows = target_table.find_all("tr")
 
-# 원하는 도시 이름과 시정(가시성)을 가져오기
-desired_city = "서울"
+    for row in rows:
+        # 각 행의 첫 번째 셀에서 도시 이름 가져오기
+        city_cell = row.find("td")
+        
+        # None 체크 추가
+        if city_cell is not None:
+            current_city_name = city_cell.text.strip()
 
-for row in rows:
-    # 각 행의 첫 번째 셀에서 도시 이름 가져오기
-    city_cell = row.find("td")
-    
-    # None 체크 추가
-    if city_cell is not None:
-        city_name = city_cell.text.strip()
-
-        # 도시 이름이 원하는 도시와 일치하는 경우
-        if city_name == desired_city:
-            # 행에서 시정(가시성) 가져오기
-            visibility_cell = row.find_all("td")[2]  # 시정(가시성)이 있는 열의 인덱스
-            
-            # None 체크 추가
-            if visibility_cell is not None:
-                visibility = visibility_cell.text.strip()
-
-                # 가져온 데이터 출력
-                print(f"도시: {city_name}, 시정(가시성): {visibility}Km")
-
-                # oneM2M 서버로 데이터 전송
-                typeIndex = 3  # templates에서 Visibility에 해당하는 인덱스
-                app.updateDevice(typeIndex, "서울", visibility)
+            # 도시 이름이 원하는 도시와 일치하는 경우
+            if current_city_name == DESIRED_CITY:
+                # 행에서 시정(가시성) 가져오기
+                visibility_cell = row.find_all("td")[2]  # 시정(가시성)이 있는 열의 인덱스
                 
-            else:
-                print(f"도시: {desired_city}의 시정(가시성) 정보를 찾을 수 없습니다.")
-            
-            break
+                # None 체크 추가
+                if visibility_cell is not None:
+                    visibility = visibility_cell.text.strip()
+                    return f"도시: {current_city_name}, 시정(가시성): {visibility}Km"
+                else:
+                    return f"도시: {DESIRED_CITY}의 시정(가시성) 정보를 찾을 수 없습니다."
+
+    return f"도시: {DESIRED_CITY}를 찾을 수 없습니다."
+
+# 원하는 도시의 가시성 정보를 가져오기
+result = get_visibility()
+print(result)
